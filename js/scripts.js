84d666a91771b65ae8a3b2ff40563dc0dd28e7f6
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-18 14:53:59 -0700

    add code to remove messages once clicked

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,33 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye</li>");
     $("ul#webpage").prepend("<li>Goodbye, human!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>No, YOU stop copying ME!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
